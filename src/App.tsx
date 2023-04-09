db1f189e220e1d15f3d79bfcb6b6d39f4f7952e8
Naoya Yamashita <conao3@gmail.com>
2023-04-09 23:51:30 +0900

    make height as 100vh

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {Button, defaultTheme, Provider} from '@adobe/react-spectrum';
 
 export function App() {
   return (
-    <Provider theme={defaultTheme} colorScheme='dark'>
+    <Provider theme={defaultTheme} colorScheme='dark' height="100%">
       <Button
         variant="accent"
         onPress={() => alert('Hey there!')}
