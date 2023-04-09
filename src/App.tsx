ecbe59ff71e5934e6bfdd4e1b0032f630810c0db
Naoya Yamashita <conao3@gmail.com>
2023-04-10 00:14:47 +0900

    add simple flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import {Button, defaultTheme, Provider} from '@adobe/react-spectrum';
+import {defaultTheme, Flex, Provider, View} from '@adobe/react-spectrum';
 
 export function App() {
   return (
     <Provider theme={defaultTheme} colorScheme='dark' height="100%">
-      <Button
-        variant="accent"
-        onPress={() => alert('Hey there!')}
-      >
-        Hello React Spectrum!
-      </Button>
+      <View padding="size-100">
+        <Flex direction="column" width="100%" maxWidth="1200px" gap="size-100" margin="0 auto">
+          <View backgroundColor="celery-600" height="size-800" />
+          <View backgroundColor="blue-600" height="size-800" />
+          <View backgroundColor="magenta-600" height="size-800" />
+        </Flex>
+      </View>
     </Provider>
   );
 }
