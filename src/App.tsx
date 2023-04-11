64c0c3bc24331fae9e012956867ee662f1837341
Naoya Yamashita <conao3@gmail.com>
2023-04-12 08:45:17 +0900

    add droppable

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { SimpleListView } from './components/SimpleListView';
 import { SimpleListViewDnd } from './components/SimpleListViewDnd';
 import { SimpleListViewDragIntoList } from './components/SimpleListViewDragIntoList';
 import { SimpleListViewDndFrom } from './components/SimpleListViewDndFrom';
+import { Draggable } from './atoms/Draggable';
+import { Droppable } from './atoms/Droppable';
 
 export function App() {
   return (
@@ -14,6 +16,19 @@ export function App() {
     >
       <View padding="size-100">
         <Flex direction="column" width="100%" maxWidth="1200px" gap="size-300" margin="0 auto">
+          <View>
+            <Heading level={1}>Atoms</Heading>
+            <Flex direction="column" gap="size-100">
+              <View>
+                <Heading level={2}>Draggable</Heading>
+                <Draggable />
+              </View>
+              <View>
+                <Heading level={2}>Droppable</Heading>
+                <Droppable />
+              </View>
+            </Flex>
+          </View>
           <View>
             <Heading level={1}>SimpleListView</Heading>
             <SimpleListView />
---
+++ src/atoms/Droppable.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type { TextDropItem } from 'react-aria';
+import { useDrop } from 'react-aria';
+
+export function Droppable() {
+  let [dropped, setDropped] = React.useState<string | null>(null);
+  let ref = React.useRef(null);
+  let { dropProps, isDropTarget } = useDrop({
+    ref,
+    async onDrop(e) {
+      let items = await Promise.all(
+        e.items
+          .filter((item) =>
+            item.kind === 'text' && item.types.has('text/plain')
+          )
+          .map((item) => (item as TextDropItem).getText('text/plain'))
+      );
+      setDropped(items.join('\n'));
+    }
+  });
+
+  return (
+    <div
+      {...dropProps}
+      ref={ref}
+      className={`droppable ${isDropTarget ? 'target' : ''}`}
+    >
+      {dropped || 'Drop here'}
+    </div>
+  );
+}
