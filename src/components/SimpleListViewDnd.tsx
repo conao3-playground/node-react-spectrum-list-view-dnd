0e19b8d9a1a245a701f4599c9e484d24eec13cbc
Naoya Yamashita <conao3@gmail.com>
2023-04-16 08:06:16 +0900

    use Droppable for SimpleListViewDnd

--- src/components/SimpleListViewDnd.tsx
+++ src/components/SimpleListViewDnd.tsx
@@ -1,9 +1,8 @@
-import { Flex, Item, ListView, View, useDragAndDrop, useListData } from "@adobe/react-spectrum";
-import React from "react";
-import type { TextDropItem } from "react-aria";
-import { useDrop } from "react-aria";
+import { Flex, Item, ListView, useDragAndDrop, useListData } from "@adobe/react-spectrum";
+import { Droppable } from "../atoms/Droppable";
 
 export function SimpleListViewDnd() {
+  const draggableId = "SimpleListViewDnd";
   const lst = useListData({
     initialItems: [
       {id: '1', name: 'Adobe Photoshop'},
@@ -13,40 +12,12 @@ export function SimpleListViewDnd() {
       {id: '5', name: 'Adobe Lightroom'},
     ]
   });
-  type DropItem = {
-    item: string
-  }
-  const [dropped, setDropped] = React.useState<DropItem[]>();
-  const ref = React.useRef(null);
-  const { dropProps, isDropTarget } = useDrop({
-    ref,
-    async onDrop(e) {
-      console.log(e)
-      const items = await Promise.all(
-        e.items.filter((item) => (
-          item.kind === 'text' &&
-          item.types.has('SimpleListViewDnd')
-        ))
-        .map(async (item) => {
-          item = item as TextDropItem
-          return {
-            item: await item.getText('text/plain')
-          }
-        })
-      );
-      setDropped(items);
-    }
-  });
   const { dragAndDropHooks } = useDragAndDrop({
     getItems: (keys) => [...keys].map((key) => ({
-      'SimpleListViewDnd': 'true',
+      [ draggableId ]: 'true',
       'text/plain': String(lst.getItem(key).name),
     })),
   });
-
-  const DroppableStyle = {
-    height: '100%',
-  }
 
   return <>
     <Flex wrap gap="size-200">
@@ -62,24 +33,7 @@ export function SimpleListViewDnd() {
           <Item key={item.id}>{item.name}</Item>
         )}
       </ListView>
-      <View
-        borderWidth="thin"
-        borderRadius="regular"
-        borderColor={isDropTarget ? "focus" : "default"}
-        backgroundColor={isDropTarget ? "informative" : "transparent"}
-        padding="size-100"
-        width="100%"
-        maxWidth="size-6000"
-        minHeight="size-2000"
-      >
-        <div
-          style={DroppableStyle}
-          ref={ref}
-          {...dropProps}
-        >
-          {(dropped && dropped.map((elm) => elm.item).join(" / ")) || 'Drop here'}
-        </div>
-      </View>
+      <Droppable draggableId={draggableId}/>
     </Flex>
   </>
 }
